0daff192da886b08dbf7255be808ff1af196e7b4
Jakub Giminski <jakub@gigabyte.software>
2019-03-06 14:27:54 +0000

    Add typehint to event store

--- src/Event/Store/EventStore.php
+++ src/Event/Store/EventStore.php
@@ -6,7 +6,7 @@ use CQRS\Event\Event;
 
 interface EventStore
 {
-    public function registerEvent($event): EventStoreId;
+    public function registerEvent(Event $event): EventStoreId;
 
     public function getById(EventStoreId $eventId): Event;
 }
