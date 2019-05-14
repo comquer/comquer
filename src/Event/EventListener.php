157554ce813228168586daa1ebeeb60054861aae
Jakub Giminski <jakub@gigabyte.software>
2019-05-14 17:39:51 +0100

    Introduce invokable event listener

---
+++ src/Event/EventListener.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Comquer\Event;
+
+use Comquer\DomainIntegration\Event\Event;
+use Comquer\NamedResource;
+
+interface EventListener extends NamedResource
+{
+    public function __invoke(Event $event) : void;
+}
