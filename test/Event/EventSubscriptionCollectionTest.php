1338b05a7f54e67353458e36f51ee601af64392d
Jakub Giminski <jakub@gigabyte.software>
2019-06-03 16:55:27 +0100

    Lock phpunit version to 8.0.0

--- test/Event/EventSubscriptionCollectionTest.php
+++ test/Event/EventSubscriptionCollectionTest.php
@@ -24,7 +24,7 @@ class EventSubscriptionCollectionTest extends TestCase
             ItemAdded::getName() => [
                 UpdateShoppingListProjection::getName(),
             ],
-      ];
+        ];
    }
 
     /** @test */
